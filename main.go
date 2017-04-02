08aec50da875f03a054e3c5bf1061e17e388de56
Mustafa Yasin HALICI <yasin@sudo.co>
2017-04-02 18:27:38 +0300

    add hodor

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"regexp"
 	"strconv"
 	"strings"
@@ -10,21 +9,21 @@ import (
 	"github.com/anaskhan96/soup"
 )
 
+func hodo(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 func main() {
 	for i := 0; i < 1786; i++ {
 		url := "http://www.wikiobd.co.uk/vehicle.php?vh=" + strconv.Itoa(i)
 		resp, err := soup.Get(url)
-		if err != nil {
-			log.Fatal(err)
-		}
-
+		hodo(err)
 		doc := soup.HTMLParse(resp)
-
 		model := doc.Find("ul", "class", "ui-corner-top ui-shadow").Find("li", "class", "ui-li-divider").Find("h3").Text()
 		year := doc.Find("ul", "class", "ui-corner-top ui-shadow").Find("li", "class", "ui-li-divider").Find("p").Text()
 		desc := doc.Find("div", "id", "description").Find("p").Text()
 		photos := doc.Find("div", "class", "content jqm-content").FindAll("img", "class", "locatorimage")
-
 		modelslice := strings.Split(model, " ")
 		fmt.Println("Brand : " + modelslice[0])
 		fmt.Printf("Model : ")
